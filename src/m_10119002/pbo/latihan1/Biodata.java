0c70e21d20ca7d4ab5e7e6767f96b279fcce73dc
fsahi <fsahi@FS-DesktopPC>
2020-10-21 01:16:34 +0700

    21 Rabu 2020

---
+++ src/m_10119002/pbo/latihan1/Biodata.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package m_10119002.pbo.latihan1;
+
+/**
+ *
+ * @author
+ * NAMA     : Firman Sahita
+ * KELAS    : PBO1
+ * NIM      : 10119002
+ * Deskripsi Program : Program ini berisi program untuk menampilkan
+ * jenis-jenis tipe data bilangan bulat
+ * 
+ */
+public class Biodata {
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "---------------------");
+        System.out.println("NAMA\t\t: Rizky Adam Kurniawan");
+        System.out.println("ALAMAT\t\t: Jalan Antapani no 89");
+        System.out.println("TEMPAT LAHIR\t: Bandung");
+        System.out.println("TANGGAL LAHIR\t: 25 Januari 1991");
+        System.out.println("---------------------");
+        System.out.println("HOBI\t\t: Renang & Lari");
+        System.out.println("MAKES\t\t: Sate");
+        System.out.println("MIKES\t\t: Cendol");
+    }
+}
